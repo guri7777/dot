7432980f549cd429545217b7b6d9763211dc075b
guri7777 <bjh1353@gmail.com>
2024-02-22 11:17:44 +0900

    추천 영상 작업

--- src/data/header.js
+++ src/data/header.js
@@ -1,126 +1,76 @@
-import { CiBaseball } from "react-icons/ci";
-import { CiCoins1 } from "react-icons/ci";
-import { CiBoxes } from "react-icons/ci";
-import { CiBullhorn } from "react-icons/ci";
-import { CiCoffeeCup } from "react-icons/ci";
-import { CiDumbbell } from "react-icons/ci";
-import { CiFries } from "react-icons/ci";
-import { CiMoneyBill } from "react-icons/ci";
+import todayView01 from "../assets/img/today/todayView01.jpg";
+import todayView02 from "../assets/img/today/todayView02.jpg";
+import todayView03 from "../assets/img/today/todayView03.jpg";
+import todayView04 from "../assets/img/today/todayView04.jpg";
+import todayView05 from "../assets/img/today/todayView05.jpg";
+import todayView06 from "../assets/img/today/todayView06.jpg";
+import todayView07 from "../assets/img/today/todayView07.jpg";
+import todayView08 from "../assets/img/today/todayView08.jpg";
 
-import { AiFillGithub } from "react-icons/ai";
-import { AiOutlineCodepen } from "react-icons/ai";
-import { AiFillYoutube } from "react-icons/ai";
-import { AiOutlineInstagram } from "react-icons/ai";
-
-export const headerMenus = [
+export const todayText = [
     {
-        title: "웹스토리보이",
-        icon: <CiBaseball />,
-        src: "/"
-    },
-    {
-        title: "추천 영상",
-        icon: <CiMoneyBill />,
-        src: "/today"
-    },
-    {
-        title: "추천 개발자",
-        icon: <CiCoins1 />,
-        src: "/developer"
-    },
-    {
-        title: "웹디자인기능사",
-        icon: <CiBoxes />,
-        src: "/webd"
-    },
-    {
-        title: "웹표준 사이트",
-        icon: <CiBullhorn />,
-        src: "/website"
-    },
-    {
-        title: "GSAP Parallax",
-        icon: <CiCoffeeCup />,
-        src: "/gsap"
-    },
-    {
-        title: "포트폴리오 사이트",
-        icon: <CiDumbbell />,
-        src: "/port"
-    },
-    {
-        title: "유튜브 클론 사이트",
-        icon: <CiFries />,
-        src: "/youtube"
-    },
-];
-
-export const searchKeyword = [
-    {
-        title: "webstoryboy",
-        src: "/search/webstoryboy"
-    },
-    {
-        title: "HTML",
-        src: "/search/html"
-    },
-    {
-        title: "CSS",
-        src: "/search/css"
-    },
-    {
-        title: "JavaScript",
-        src: "/search/javascript"
-    },
-    {
-        title: "React.js",
-        src: "/search/react.js"
-    },
-    {
-        title: "Vue.js",
-        src: "/search/vue.js"
-    },
-    {
-        title: "Next.js",
-        src: "/search/next.js"
-    },
-    {
-        title: "Node.js",
-        src: "/search/node.js"
-    },
-    {
-        title: "SQL",
-        src: "/search/sql"
-    },
-    {
-        title: "portfolio",
-        src: "/search/React Portfolio"
-    },
-    {
-        title: "music",
-        src: "/search/NewJeans"
-    }
-];
-
-export const snsLink = [
-    {
-        title: "github",
-        url: "https://github.com/webstoryboy",
-        icon: <AiFillGithub />
-    },
-    {
-        title: "youtube",
-        url: "https://www.youtube.com/webstoryboy",
-        icon: <AiFillYoutube />
-    },
-    {
-        title: "codepen",
-        url: "https://codepen.io/webstoryboy",
-        icon: <AiOutlineCodepen />
-    },
-    {
-        title: "instagram",
-        url: "https://www.instagram.com/webstoryboy",
-        icon: <AiOutlineInstagram />
+        img: todayView01,
+        title: "나만의 코딩 유튜브 사이트 만들기 프로젝트",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/youtube"
+    },{
+        img: todayView02,
+        title: "Vite를 이용하여 포트폴리오 사이트 만들기",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView03,
+        title: "React.js를 이용하여 포트폴리오 사이트 만들기",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView04,
+        title: "Vue.js를 이용하여 포트폴리오 사이트 만들기",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView05,
+        title: "Next.js를 이용하여 포트폴리오 사이트 만들기",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/port"
+    },{
+        img: todayView06,
+        title: "GSAP를 이용하여 창의적인 사이트 만들기",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/gsap"
+    },{
+        img: todayView07,
+        title: "웹디자인 기능사는 이 영상으로 준비 끝",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/webd"
+    },{
+        img: todayView08,
+        title: "디자인 부터 코딩까지 준비 완료",
+        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+        author : "webstoryboy",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/website"
     },
 ]
